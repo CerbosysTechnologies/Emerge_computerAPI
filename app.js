8e936ef4a86b21adb727bf06b62359cbc8f68f59
CerbosysTechnologies <93573082+CerbosysTechnologies@users.noreply.github.com>
2023-02-03 12:38:23 +0530

    3feb

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express= require('express');
 const cors = require('cors');
-const pool = require('./authorization/config');
+const pool = require('./authorization/config.js');
+const router = require('./src/routes/routes.js')
+
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use("/emerge",router);
+
+
 
 const port=4020
+
 app.listen(port,()=>{
-    console.log("server runing at"+port)
+    console.log("server runing at  = "+port)
 })
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,1 +1,16 @@
+const express =require('express')
+const logincontroller=  require('../controllers/login.controller.js')
+
+
+
+const router =express.Router()
+
+
+router.post('/login',logincontroller.createlogin)
+
 // there is routs 
+
+
+
+
+module.exports= router;
